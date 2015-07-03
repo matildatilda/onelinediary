cfea817d8ac450574fc8cd4781a9c3ee25eb4016
matildatilda <melty.bombom@gmail.com>
2015-07-03 08:29:44 +0000

    2.0

--- php/getDiaries.php
+++ php/getDiaries.php
@@ -21,24 +21,12 @@
     if (!$result) {
         die("Query failed. (" .mysql_error(). ")\n");
     }
-    
-    /*
-    printf("{");
-    while ($row = $result->fetch_row()) {
-        printf("{");
-        printf (" year: %d,", $row[1]);
-        printf(" month: %d,", $row[2]);
-        printf(" day: %d,", $row[3]);
-        printf(" diary: %s,",$row[4]);
-        printf(" }");
-    }
-    printf(" }");
-    */
-    printf("<tr><th>日</th><th>日記</th><th>変更</th><th>削除</th></tr>");
+  
+    printf("<tr><th>日</th><th>日記</th><th>変更</th></tr>");
 
     while ($row = $result->fetch_row())
     {
-        printf("<tr><th>".$row[3]."</td><td>".$row[4]."</td><td></td><td></td><tr>");    
+        printf("<tr><td>".$row[3]."</td><td>".$row[4]."</td><td><a href='./writeDiary.html?diary_id=".$row[0]."'>変更</a></td></tr>");    
     }    
     $result->close();
     $mysqli->close();
